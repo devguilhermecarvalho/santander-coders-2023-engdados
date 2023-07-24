7b002b3a51af67d380080a686501a7eb375837b3
Guilherme Carvalho <guilhermerdcarvalho@gmail.com>
2023-07-24 16:48:43 -0300

    Atualização

---
+++ Lógica de Programacao/Projeto - Logica de Programacao II/teste-projeto.py
@@ -0,0 +1,15 @@
+def menu():
+    print("""
+        Boas-vindas ao nosso sistema:
+            
+        1 – Inserir usuário
+        2 – Excluir usuário
+        3 – Atualizar usuário
+        4 – Informações de um usuário
+        5 – Informações de todos os usuários
+        6 – Sair
+        """)
+    escolha = abs(int(input("Qual opção desejada?")))
+    
+
+menu()
