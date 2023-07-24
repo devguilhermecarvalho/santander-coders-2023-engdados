44ec2f4babf63abac0aeb2d2a543e12d96b3ae95
Guilherme Rosa de Carvalho <79029084+devguilhermecarvalho@users.noreply.github.com>
2023-07-24 14:50:49 -0300

    Add files via upload

--- Exercicios/03_Lista_Everest.py
+++ Exercicios/03_Lista_Everest.py
@@ -1,20 +1,24 @@
 """ Subtraindo elementos usando funções de alta ordem
+
 Em programação, temos que pensar não apenas na implementação do código propriamente dita para execução correta da tarefa desejada, como também na melhor forma de realizar esta implementação. Com isso, paradigmas de programação foram criados para auxiliar o programador a pensar diferente.
 
 Um desses paradigmas é o uso de funções de alta ordem, o que permite que realizemos diversas operações em coleções (listas, tuplas, arrays, etc) sem o uso de loops explicitamente.
 
-Um dos usos mais comuns é o uso de funções reduce, responsáveis por acumular uma operação ao longo de uma coleção. Sabendo disso, implemente uma função reduceSub() que recebe uma lista, e então faz uso do reduce para realizar a subtração dos números da lista com base no valor inicial 3."""
+Um dos usos mais comuns é o uso de funções reduce, responsáveis por acumular uma operação ao longo de uma coleção. Sabendo disso, implemente uma função reduceSub() que recebe uma lista, e então faz uso do reduce para realizar a subtração dos números da lista com base no valor inicial 3.
+"""
 
-listaA = list(range(1, 1001))
+from functools import reduce
 
-def numerosDiv7(listaA):
-    div7 = [num for num in listaA if num % 7 == 0]
-    numeros_unicos = list(set(div7))
-    return numeros_unicos
+lista = list(range(1, 31))
 
-resultado = numerosDiv7(listaA)
+def subtrair(a, b):
+    return a - b
 
-if resultado:
-    print(resultado)
-else:
-    print("Não números divisíveis por 7 na lista.")
+def reduceSub(lista):
+    valor_inicial = 3
+    resultadoReduce = reduce(subtrair, lista, valor_inicial)
+    return resultadoReduce
+
+resultado = reduceSub(lista)
+
+print(resultado)
