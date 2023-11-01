5c3b0d5be9a9f7ea13dbc2a0b9aaf8bdf65b07ea
devguilhermecarvalho <guii_carvalho@hotmail.com>
2023-11-01 17:06:43 -0300

    'att'

--- 05-Tratamento_de_Dados/Projeto/02-camada_silver/Readme.md
+++ 05-Tratamento_de_Dados/Projeto/02-camada_silver/Readme.md
@@ -2,6 +2,6 @@
 
 **dbt_project_ada** - Ambiente da instalação do DBT e do Great Expactation como ferramenta do DBT.
 
-- O intuito dessa camada foi realizar testes e verificações nas colunas do dataframe e apartir disso salvar no banco de dados um novo dataframe desta etapa (df_nome_silver).
-  -  Não foquei no tratamento dos dados, mas em compreender o funcionamento do DBT.
-  -  O DBT é bem diferente do Python, então parece ser mais simples a utilização do Python para verificações e correções no dataframe por conta da falta de costume na utilização do DBT.
+* O intuito dessa camada foi realizar testes e verificações nas colunas do dataframe e apartir disso salvar no banco de dados um novo dataframe desta etapa (df_nome_silver).
+  * Não foquei no tratamento dos dados, mas em compreender o funcionamento do DBT.
+  * O DBT é bem diferente do Python, então parece ser mais simples a utilização do Python para verificações e correções no dataframe por conta da falta de costume na utilização do DBT.
---
+++ 05-Tratamento_de_Dados/Projeto/02-camada_silver/Tipos das colunas.ps1
@@ -0,0 +1,20 @@
+ #   Column                          Non-Null Count  Dtype  Newtype
+---  ------                          --------------  -----  -------
+ 0   id                              464 non-null    int64      ok
+ 1   name                            464 non-null    object     ok
+ 2   host_id                         464 non-null    int64      ok
+ 3   host_name                       464 non-null    object     ok
+ 4   neighbourhood_group             0 non-null      object     ok
+ 5   neighbourhood                   464 non-null    object     ok
+ 6   latitude                        464 non-null    float64    ok
+ 7   longitude                       464 non-null    float64    ok
+ 8   room_type                       464 non-null    object     ok
+ 9   price                           464 non-null    int64      ok
+ 10  minimum_nights                  464 non-null    int64      ok
+ 11  number_of_reviews               464 non-null    int64      ok
+ 12  last_review                     418 non-null    object     MUDAR PARA DATA 
+ 13  reviews_per_month               418 non-null    float64    ok
+ 14  calculated_host_listings_count  464 non-null    int64      ok  
+ 15  availability_365                464 non-null    int64      ok 
+ 16  number_of_reviews_ltm           464 non-null    int64      ok
+ 17  license                         0 non-null      object     ok
